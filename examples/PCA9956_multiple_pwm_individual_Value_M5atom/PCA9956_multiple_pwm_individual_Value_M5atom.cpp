9b48c70600678db6e15d2879ec889f99a3adffe0
Yuske Goto <yuskegoto@gmail.com>
2020-12-14 12:13:43 +0900

    Added individual PWM coding sample

---
+++ examples/PCA9956_multiple_pwm_individual_Value_M5atom/PCA9956_multiple_pwm_individual_Value_M5atom.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include "PCA9956.h"
+#include "Setting.h"
+
+#define M5ATOM
+
+#ifdef M5ATOM
+#include "M5Atom.h"
+#endif
+
+PCA9956 ledDrivers[2]{{&Wire}, {&Wire}}; // if you have multiple drivers, init this way...
+
+#ifdef M5ATOM
+bool blink = false;
+#endif
+
+void setup()
+{
+#ifdef M5ATOM
+    M5.begin(true, false, true); // seems like m5 inits wire, which is not comvenient so disables it first
+    delay(50);
+    M5.dis.drawpix(0, 0x000000);
+#else
+    Serial.begin(9600);
+#endif
+
+    Wire.begin(21, 22); // Wire needs to be initialized separately...
+
+    ledDrivers[0].init(DEV_ADDRESS1, LED_BRIGHTNESS);
+    ledDrivers[1].init(DEV_ADDRESS2, LED_BRIGHTNESS);
+}
+
+void loop()
+{
+    /*********************** simple ON-OFF control demo *************************************/
+
+    for (uint8_t j = 0; j < PCA9965_NUM_LEDS; j++)
+    {
+        for (uint8_t i = 0; i < PCA9965_NUM_LEDS; i++)
+        {
+            float val = -cos((float)(i + j) / PCA9965_NUM_LEDS * PI * 2) + 1;
+            val *= 128;
+            if (val > 255)
+                val = 0xFF;
+            else if (val < 0)
+                val = 0;
+            uint8_t val_int = (uint8_t)val;
+            // Serial.print(val);
+            // Serial.print(" ");
+            ledDrivers[0].pwmLED(i, val_int);
+            ledDrivers[1].pwmLED(PCA9965_NUM_LEDS - i - 1, val_int);
+        }
+        // Serial.println();
+
+#ifdef M5ATOM
+        if (blink)
+            M5.dis.drawpix(0, 0x000000);
+        else
+            M5.dis.drawpix(0, 0xf00000);
+#endif
+        blink = !blink;
+        delay(20);
+    }
+}
---
+++ examples/PCA9956_multiple_pwm_individual_Value_M5atom/Setting.h
@@ -0,0 +1,30 @@
+// i2c addresses
+#define DEV_ADDRESS1 0x3D
+#define DEV_ADDRESS2 0x3F
+// #define DEV_ADDRESS3 0x40
+// #define DEV_ADDRESS4 0x41
+// #define DEV_ADDRESS5 0x42
+// #define DEV_ADDRESS6 0x43
+// #define DEV_ADDRESS7 0x44
+// #define DEV_ADDRESS8 0x45
+// #define DEV_ADDRESS9 0x46
+// #define DEV_ADDRESS10 0x47
+
+// const uint8_t DEV1_Group[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1};
+// const uint8_t DEV1_ledNo[] =  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+// const uint8_t DEV2_Group[] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2,  2,  2,  2,  2,  2,  2, 2,   2, 3, 3, 3, 3, 3, 3};
+// const uint8_t DEV2_ledNo[] =  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0, 1, 2, 3, 4, 5};
+
+// const uint8_t DEV3_Group[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4, 5, 5, 5, 5};
+// const uint8_t DEV3_ledNo[] =  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3};
+// const uint8_t DEV4_Group[] = {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+// const uint8_t DEV5_Group[] = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+// const uint8_t DEV6_Group[] = {8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8};
+// const uint8_t DEV7_Group[] = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+// const uint8_t DEV8_Group[] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+// const uint8_t DEV9_Group[] = {11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11};
+// const uint8_t DEV10_Group[] = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+
+// led control settings
+#define LED_BRIGHTNESS 0x0F
